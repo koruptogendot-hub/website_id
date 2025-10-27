e67f6629e71e9b0f1a62303bc1afc6dc20f3383b
koruptogendot-hub <koruptogendot@gmail.com>
2025-10-27 16:55:21 +0700

    Add files via upload

---
+++ pages/maintance.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="/style.css">
+<div class="cyber-form">
+  <h2>🧰 Maintenance Mode</h2>
+  <p style="text-align:center;">Fitur ini sedang dalam perbaikan.<br>Silakan gunakan menu <b>SEND GMAIL</b> terlebih dahulu.</p>
+</div>
